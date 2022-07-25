f202312a553bb3e4617a01ce45454a3e6a18dcac
jeff <36491222+jeffprideandjoy@users.noreply.github.com>
2022-07-25 17:32:17 +1200

    Add files via upload

---
+++ Runtime/Scripts/Networking/API/GetAllScenarios.cs
@@ -0,0 +1,40 @@
+﻿using SkillsVR.EnterpriseCloudSDK.Data;
+using System.Collections.Generic;
+
+namespace SkillsVR.EnterpriseCloudSDK.Networking.API
+{
+    public partial class GetAllScenarios : AbstractAPI<AbstractAPI.EmptyData, GetConfig.Response>
+    {
+        public GetAllScenarios() : base(string.Format(ECAPI.domain + "/api/scenario/all"))
+        {
+            requestType = HttpRequestType.GET;
+            authenticated = true;
+        }
+
+        [System.Serializable]
+        public partial class Response : BaseResponse
+        {
+            public int code;
+            public int message;
+            public List<ScenarioData> data;
+        }
+
+        [System.Serializable]
+        public class ScenarioData
+        {
+            public string name;
+            public string version;
+            public string description;
+
+            public List<ApkConfig> apkFiles;
+        }
+
+        public class ApkConfig
+        {
+            public string name;            
+            public string sas;
+            public int scenarioId;
+            public string device;
+        }
+    }
+}

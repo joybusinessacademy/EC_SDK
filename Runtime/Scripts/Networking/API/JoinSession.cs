8bb9a339e1ac57de6d5e67d4b2fd6b7c7f12aa4a
jeff <36491222+jeffprideandjoy@users.noreply.github.com>
2022-10-07 13:17:10 +1300

    create and joi api

---
+++ Runtime/Scripts/Networking/API/JoinSession.cs
@@ -0,0 +1,20 @@
+﻿using SkillsVR.EnterpriseCloudSDK.Data;
+
+namespace SkillsVR.EnterpriseCloudSDK.Networking.API
+{
+    public partial class JoinSession : AbstractAPI<AbstractAPI.EmptyData, JoinSession.Response>
+    {
+        public JoinSession(int scenarioId, string pinCode) : base(string.Format(ECAPI.domain + "/api/plannedsession/join/{0}/{1}", scenarioId.ToString(), pinCode))
+        {
+            requestType = HttpRequestType.POST;
+            authenticated = true;
+        }
+
+        [System.Serializable]
+        public partial class Response : BaseResponse
+        {
+            public int code;
+            public int message;
+        }
+    }
+}

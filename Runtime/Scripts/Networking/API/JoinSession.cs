5947840edb7a1e71b9bfeee104dcf2e89c5f82f6
jeff <36491222+jeffprideandjoy@users.noreply.github.com>
2022-10-13 10:57:01 +1300

    Update JoinSession.cs

--- Runtime/Scripts/Networking/API/JoinSession.cs
+++ Runtime/Scripts/Networking/API/JoinSession.cs
@@ -2,12 +2,18 @@
 
 namespace SkillsVR.EnterpriseCloudSDK.Networking.API
 {
-    public partial class JoinSession : AbstractAPI<AbstractAPI.EmptyData, CreateSession.Response>
+    public partial class JoinSession : AbstractAPI<JoinSession.Data, CreateSession.Response>
     {
         public JoinSession(int scenarioId, string pinCode) : base(string.Format(ECAPI.domain + "/api/plannedsession/join/{0}/{1}", scenarioId.ToString(), pinCode))
         {
             requestType = HttpRequestType.POST;
             authenticated = true;
+        }
+
+        [System.Serializable]
+        public class Data
+        {
+            public string deviceId = SystemInfo.deviceUniqueIdentifier;
         }
     }
 }

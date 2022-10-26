daafad23756976cf682925f786866fb65ef860c7
jeff <36491222+jeffprideandjoy@users.noreply.github.com>
2022-10-27 11:33:59 +1300

    Update ECRecordCollectionAsset.cs

--- Runtime/Scripts/Data/ECRecordCollectionAsset.cs
+++ Runtime/Scripts/Data/ECRecordCollectionAsset.cs
@@ -162,7 +162,7 @@ namespace SkillsVR.EnterpriseCloudSDK.Data
 
         public void SubmitUserScore(Action<AbstractAPI.EmptyResponse> success = null, Action<string> failed = null)
         {
-            TryLoginThen(() => { ECAPI.SubmitUserLearningRecord(currentConfig.scenarioId, currentConfig.durationMS, currentConfig.managedRecords, success, failed); }, failed);
+            TryLoginThen(() => { ECAPI.SubmitUserLearningRecord(currentConfig.scenarioId, currentConfig.durationMS, currentConfig.managedRecords, currentConfig.skillRecords, success, failed); }, failed);
         }
 
         public void TryLoginThen(Action actionAfterLogin, Action<string> onError)

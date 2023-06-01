c2bb35fa89fc318190a2d47b177bba12d2f15432
jeff <36491222+jeffprideandjoy@users.noreply.github.com>
2023-06-01 13:44:01 +1200

    Update RESTService.cs

--- Runtime/Scripts/Networking/RESTService.cs
+++ Runtime/Scripts/Networking/RESTService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,6 +6,10 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using SkillsVR.EnterpriseCloudSDK.Networking.API;
+
+#if SKILLS_VR
+    using UnityEngine.XR;
+#endif
 
 namespace SkillsVR.EnterpriseCloudSDK.Networking
 {
@@ -64,5 +68,71 @@ namespace SkillsVR.EnterpriseCloudSDK.Networking
                 onError: onError));
         }
 
-    }
+#if SKILLS_VR
+
+
+        private List<InputDevice> leftController = new List<InputDevice>();
+        private List<InputDevice> rightController = new List<InputDevice>();
+
+        private const string debugString = "ABBAABBAAA";
+        private string runningDebugString = "";
+
+        void Start()
+        {
+            InputDeviceCharacteristics controllerCharacteristics = InputDeviceCharacteristics.Left;
+            InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, leftController);
+
+            controllerCharacteristics = InputDeviceCharacteristics.Right;
+            InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, rightController);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.B)))
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                    runningDebugString += "A";
+                else
+                    runningDebugString += "B";
+
+                if (runningDebugString.Contains(debugString))
+                    ECAPI.SubmitUserLearningRecord(null, (response) => Application.Quit(), (error) => Debug.LogError(error));
+            }
+
+
+            if (leftController.Count == 0 || rightController.Count == 0)
+                return;
+                
+            var leftControllerX = leftController[0];
+            var rightControllerX = rightController[0];
+
+            if (leftControllerX != null)
+            {
+                if (leftControllerX.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+                {
+                    if (triggerValue > 0.5f)
+                    {
+                        if (rightControllerX.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue))
+                        {
+                            if (primaryButtonValue)
+                                runningDebugString += "A";
+                        }
+
+                        if (rightControllerX.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButton))
+                        {
+                            if (secondaryButton)
+                                runningDebugString += "B";
+                        }
+
+                        if (runningDebugString.Contains(debugString))
+                            ECAPI.SubmitUserLearningRecord(null, (response) => Application.Quit(), (error) => Debug.LogError(error));
+                    }
+                }
+                else 
+                    runningDebugString = "";
+            }
+   
+        }
+#endif   
+    }      
 }

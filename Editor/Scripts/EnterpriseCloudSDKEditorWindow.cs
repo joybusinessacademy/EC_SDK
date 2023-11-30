0a4b51606cf9ac3cd58fd9a29c6b4a3c4744bceb
AlexHome <alexb@skillsvr.com>
2023-11-30 20:14:50 +1300

    Removed Uneeded UI

--- Editor/Scripts/EnterpriseCloudSDKEditorWindow.cs
+++ Editor/Scripts/EnterpriseCloudSDKEditorWindow.cs
@@ -91,21 +91,21 @@ namespace SkillsVR.EnterpriseCloudSDK.Editor
             }
             GUI.enabled = interactable;
 
-            recordAsset.currentConfig.scenarioId = EditorGUILayout.TextField("Scenario ID:", recordAsset.currentConfig.scenarioId);
+            //recordAsset.currentConfig.scenarioId = EditorGUILayout.TextField("Scenario ID:", recordAsset.currentConfig.scenarioId);
 
-            if (ECAPI.HasLoginToken())
-            {
-                GUI.enabled = interactable && !string.IsNullOrEmpty(recordAsset.currentConfig.scenarioId);
-                if (GUILayout.Button("Get Config"))
-                {
-                    SendGetConfig();
-                }
-                GUI.enabled = interactable;
-            }
-            else
-            {
-                EditorGUILayout.HelpBox("Display records from local cached . Please login to enable submit and more functions.", MessageType.Warning);
-            }
+            //if (ECAPI.HasLoginToken())
+            //{
+            //    GUI.enabled = interactable && !string.IsNullOrEmpty(recordAsset.currentConfig.scenarioId);
+            //    if (GUILayout.Button("Get Config"))
+            //    {
+            //        SendGetConfig();
+            //    }
+            //    GUI.enabled = interactable;
+            //}
+            //else
+            //{
+            //    EditorGUILayout.HelpBox("Display records from local cached . Please login to enable submit and more functions.", MessageType.Warning);
+            //}
 
             if (recordAsset.currentConfig.managedRecords.Count > 0)
             {

37adec229070cc6c797df3ca7480ef0cc0ece917
yiding_JBA <yiding@prideandjoy.org>
2024-08-09 16:56:51 +1200

    Obsolete some ecagent methods. But not delete cause some project may still use them.

--- Runtime/Scripts/ECRecordAgent.cs
+++ Runtime/Scripts/ECRecordAgent.cs
@@ -359,21 +359,25 @@ namespace SkillsVR.EnterpriseCloudSDK
         private string setScoreId;
         private bool setScoreValue;
 
+        [Obsolete("Use ECSetGameScoreTrue(id) or ECSetGameScoreFalse(id) for quick action.")]
         public void ECSetScoreIdAction(string id)
         {
             setScoreId = id;
         }
 
-
+        [Obsolete("Use ECSetGameScoreTrue(id) or ECSetGameScoreFalse(id) for quick action.")]
         public void ECSetScoreValueAction(bool value)
         {
             setScoreValue = value;
         }
 
+        [Obsolete("Use ECSetGameScoreTrue(id) or ECSetGameScoreFalse(id) for quick action.")]
         public void ECSetScoreInvokeAction()
         {
             ECSetGameScoreBool(setScoreId, setScoreValue);
         }
+
+        [Obsolete("Use ECGetScoreInvokeAction(id) for quick action.")]
         public void ECGetScoreInvokeAction()
         {
             bool value = ECGetGameScoreBool(setScoreId);

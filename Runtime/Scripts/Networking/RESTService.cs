1cfd3cb0f53181072f09a2f66a9bf0ff5d1e6c9f
YiDing <yiding@skillsvr.com>
2024-09-05 09:13:42 +1200

    Revert "Remove debug cheat code"
    This reverts commit 704cc700a5ef7d3dfa8778c5586acc2af38ca192.

--- Runtime/Scripts/Networking/RESTService.cs
+++ Runtime/Scripts/Networking/RESTService.cs
@@ -1,7 +1,15 @@
-using SkillsVR.EnterpriseCloudSDK.Networking.API;
 using System;
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
+using SkillsVR.EnterpriseCloudSDK.Networking.API;
+
+#if SKILLS_VR
+    using UnityEngine.XR;
+#endif
 
 namespace SkillsVR.EnterpriseCloudSDK.Networking
 {
@@ -81,5 +89,101 @@ namespace SkillsVR.EnterpriseCloudSDK.Networking
                 onSuccess: onSuccess, 
                 onError: onError));
         }
+
+#if SKILLS_VR
+
+
+        private List<InputDevice> leftController = new List<InputDevice>();
+        private List<InputDevice> rightController = new List<InputDevice>();
+
+        private const string debugLearningRecordString = "ABAABBAAA";
+        private const string debugCompleteStatuString = "BABBBA";
+
+        private string runningDebugString = "";
+
+        private float timeStampADown = -1;
+        private float timeStampBDown = -1;
+        
+        IEnumerator Start()
+        {
+            while (leftController.Count == 0 || rightController.Count == 0)
+            {
+                InputDeviceCharacteristics controllerCharacteristics = InputDeviceCharacteristics.Left;
+                InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, leftController);
+    
+                controllerCharacteristics = InputDeviceCharacteristics.Right;
+                InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, rightController);
+                yield return null;
+            }
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.B)))
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                    runningDebugString += "A";
+                else
+                    runningDebugString += "B";
+
+                CheckDebugString();
+            }
+
+
+            if (leftController.Count == 0 || rightController.Count == 0)
+                return;
+                
+            var leftControllerX = leftController[0];
+            var rightControllerX = rightController[0];
+
+            if (leftControllerX != null && rightControllerX != null)
+            {
+                leftControllerX.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+                
+                if (triggerValue > 0.5f)
+                {
+                    rightControllerX.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+                    if (primaryButtonValue && timeStampADown == -1)
+                        timeStampADown = Time.time; // ondown
+
+                    if (primaryButtonValue == false && timeStampADown != -1) 
+                    {
+                        timeStampADown = -1;
+                        runningDebugString += "A";
+                    }
+
+                    rightControllerX.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButton);
+                    if (secondaryButton && timeStampBDown == -1)
+                        timeStampBDown = Time.time; // on down
+
+                    if (secondaryButton == false && timeStampBDown != -1) 
+                    {
+                        timeStampBDown = -1;
+                        runningDebugString += "B";
+                    }
+
+                    CheckDebugString();
+
+                }
+                else 
+                    runningDebugString = "";
+            }
+        }
+
+        private void CheckDebugString()
+        {
+            if (runningDebugString.Contains(debugLearningRecordString))
+            {
+                ECAPI.SubmitUserLearningRecord(null, (response) => Application.Quit(), (error) => Debug.LogError(error));
+                runningDebugString = "";
+            }
+
+            if (runningDebugString.Contains(debugCompleteStatuString))
+            {
+                ECAPI.UpdateCurrentSessionStatus(UpdateSessionStatus.Status.Completed,  (response) => Application.Quit(), (error) => Debug.LogError(error));
+                runningDebugString = "";
+            }
+        }
+#endif   
     }      
 }
